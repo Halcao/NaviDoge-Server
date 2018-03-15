050d46a3b20d5dee01dca067eb345e2ea3b63a25
XinNoil <xinwojiushiwokun@163.com>
2018-03-15 12:55:54 +0800

    modi floorplan path

--- router/floorplan.js
+++ router/floorplan.js
@@ -4,7 +4,9 @@ var fs = require("fs");
 const floorplan = function(req, res) {
     //var filename = req.body.filename;
     var filename = "55_5.svg";
-    var basePath = "/home/noil/NaviDoge-Server-master/resources/";
+    //var basePath = "/home/noil/NaviDoge-Server-master/resources/";
+    var path=require("path");
+    var basePath=path.resolve(__dirname,"..")+"/resources/";
     // 异步读取
     fs.readFile(basePath + filename, function (err, data) {
         if (err) {
