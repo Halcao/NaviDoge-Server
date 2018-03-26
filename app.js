6707a414a199e133a664f852cbdc0b4c6fdb45bb
Halcao <nyz1500@gmail.com>
2018-03-26 13:54:20 +0800

    allow * origin

--- app.js
+++ app.js
@@ -11,6 +11,14 @@ app.use(session({
   cookie: { maxAge: 60 * 1000 }
 }));
 
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+});
+
 // parse application/x-www-form-urlencoded
 // var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
